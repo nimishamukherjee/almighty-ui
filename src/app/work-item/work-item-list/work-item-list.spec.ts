e36d37113cd5ab0ba2c2a0381f54e69c098deb41
Nimisha <nimisha@dhcp35-116.lab.eng.blr.redhat.com>
2016-09-08 22:25:41 +0530

    WIP UI Unit test setup

--- src/app/work-item/work-item-list/work-item-list.spec.ts
+++ src/app/work-item/work-item-list/work-item-list.spec.ts
@@ -1,17 +1,6 @@
-//import { addProviders, inject } from '@angular/core/testing';
-import { WorkItemListComponent } from './work-item-list.component';
-
-describe('WorkItemListComponent', () => {
-  /*beforeEach(() => addProviders([
-    WorkItemListComponent
-  ]));*/
-
-  /*it('should log ngOnInit', inject([ WorkItemListComponent ], (workItemList:WorkItemListComponent) => {
-    spyOn(console, 'log');
-    expect(console.log).not.toHaveBeenCalled();
-    workItemList.ngOnInit();
-    expect(console.log).toHaveBeenCalled();
-  }));*/
-
-
+describe("1st Tests", () => {
+  it("true is true", () => expect(true).toEqual(true));
+  it("null is not the same thing as undefined", () => {
+    expect(null).not.toEqual(undefined);
+  });
 });
